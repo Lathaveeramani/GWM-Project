62e70a5ed0dac13f9d37e6d4472897636151f995
lathaveeramani1220@gmail.com <lathav@phillip.com.sg>
2022-04-04 20:36:38 +0800

    My project

---
+++ A/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lathav
+ *
+ */
+module org {
+}
---
+++ Demo/src/gWM/Sample.java
@@ -0,0 +1,14 @@
+package gWM;
+
+public class Sample {
+	
+	public Sample(){
+		System.out.println("name");
+		
+	}
+public static void main(String[]args)
+{
+	Sample s1 = new Sample();
+	
+}
+}
